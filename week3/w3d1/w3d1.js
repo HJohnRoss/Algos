9e4342e532e8b81e1370fe595ec3430dd04dbf84
FJohnHRoss <rossjohn936@gmail.com>
2023-02-20 20:18:07 -0800

    a

--- week3/w3d1/w3d1.js
+++ week3/w3d1/w3d1.js
@@ -16,5 +16,19 @@
 
 
 var mergeTwoLists = function (list1, list2) {
-  // do this problem in (leetcode) https://leetcode.com/problems/merge-two-sorted-lists/
+  if (list1 === null) {
+    return list2
+  }
+  else if (list2 === null) {
+    return list1
+  }
+
+  if (list2.val > list1.val) {
+    list1.next = mergeTwoLists(list1.next, list2)
+    return list1
+  }
+  else {
+    list2.next = mergeTwoLists(list1, list2.next)
+    return list2
+  }
 };
