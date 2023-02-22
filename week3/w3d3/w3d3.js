b22d1977b87fb3b87ea3c784d3b0dc6d823ee7d5
FJohnHRoss <rossjohn936@gmail.com>
2023-02-21 19:25:59 -0800

    a

---
+++ week3/w3d3/w3d3.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ * 
+ * Given the root of a binary tree, invert the tree, and return its root.
+ * 
+ * https://leetcode.com/problems/invert-binary-tree/
+ * Solution: https://youtu.be/OnSn2XEQ4MY
+ * 
+ * 
+ */
+
+// do inside of leetcode
+var invertTree = function (root) {
+
+};
+
