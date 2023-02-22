489faf12dcfb17d9cb6b347de28119e65b0aedd4
FJohnHRoss <rossjohn936@gmail.com>
2023-02-22 08:31:32 -0800

    w3d3

--- week3/w3d3/w3d3.js
+++ week3/w3d3/w3d3.js
@@ -20,5 +20,13 @@
 
 // do inside of leetcode
 var invertTree = function (root) {
-  
+  if (root) {
+    let temp = root.left
+    root.left = root.right
+    root.right = temp
+
+    invertTree(root.left)
+    invertTree(root.right)
+  }
+  return root
 };
