239b29425bc2423f44db21d516df148f2a9b8420
FJohnHRoss <rossjohn936@gmail.com>
2023-02-23 22:14:44 -0800

    algos

---
+++ week3/w3d5/w4d5.py
@@ -0,0 +1,24 @@
+#  * Definition for a binary tree node.
+#  * function TreeNode(val, left, right) {
+#  *     this.val = (val===undefined ? 0 : val)
+#  *     this.left = (left===undefined ? null : left)
+#  *     this.right = (right===undefined ? null : right)
+#  * }
+#  * @param {TreeNode} root
+#  * @return {number}
+#  * 
+#  * Given the root of a binary tree, return the length of the diameter of the tree.
+#  * 
+#  * The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+#  * 
+#  * The length of a path between two nodes is represented by the number of edges between them.
+#  * 
+#  * https://leetcode.com/problems/diameter-of-binary-tree
+#  * solution: https://youtu.be/bkxqA8Rfv04
+
+
+def diameterOfBinaryTree(root):
+  """
+  Solve this here
+  https://leetcode.com/problems/diameter-of-binary-tree
+  """
