fcfc31440f083cbab2dc7652526faa1d05021319
FJohnHRoss <rossjohn936@gmail.com>
2023-02-26 18:35:59 -0800

    w4d1

---
+++ week4/w4d1/w4d1.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ * 
+ * solution: https://youtu.be/QfJsau0ItOY
+ * https://leetcode.com/problems/balanced-binary-tree/
+ */
+var isBalanced = function(root) {
+    // do in leetcode https://leetcode.com/problems/balanced-binary-tree/
+};
---
+++ week4/w4d1/w4d1.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# solution: https://youtu.be/QfJsau0ItOY
+# https://leetcode.com/problems/balanced-binary-tree/
+class Solution(object):
+  def isBalanced(self, root):
+    """
+    Do in leetcode: https://leetcode.com/problems/balanced-binary-tree/
+    :type root: TreeNode
+    :rtype: bool
+    """
