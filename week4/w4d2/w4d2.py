5c608a29b173fdef6ade81f24799991ad9024654
FJohnHRoss <rossjohn936@gmail.com>
2023-02-27 23:58:04 -0800

    w4d2

---
+++ week4/w4d2/w4d2.py
@@ -0,0 +1,20 @@
+#  * Definition for a binary tree node.
+#  * function TreeNode(val, left, right) {
+#  *     this.val = (val===undefined ? 0 : val)
+#  *     this.left = (left===undefined ? null : left)
+#  *     this.right = (right===undefined ? null : right)
+#  * }
+#
+#
+#  * @param {TreeNode} p
+#  * @param {TreeNode} q
+#  * @return {boolean}
+#  * 
+#  * solution: https://youtu.be/vRbbcKXCxOw
+#  * https://leetcode.com/problems/same-tree/
+
+
+def isSameTree(p, q):
+  """
+  do inside of leetcode: https://leetcode.com/problems/same-tree/
+  """
