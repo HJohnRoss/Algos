938e1783e0bf5b2bf2028ffb3439d370067c9d8b
FJohnHRoss <rossjohn936@gmail.com>
2023-02-28 21:57:38 -0800

    w4d3

---
+++ week4/w4d3/w4d3.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ * 
+ * Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of
+ * subRoot and false otherwise.
+ * 
+ * A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be
+ * considered as a subtree of itself.
+ * 
+ * https://leetcode.com/problems/subtree-of-another-tree/
+ * solution: https://youtu.be/E36O5SWp-LE
+ */
+var isSubtree = function(root, subRoot) {
+  // do here: https://leetcode.com/problems/subtree-of-another-tree/
+};
---
+++ week4/w4d3/w4d3.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#
+# https://leetcode.com/problems/subtree-of-another-tree/
+# solution: https://youtu.be/E36O5SWp-LE
+class Solution(object):
+  def isSubtree(self, root, subRoot):
+    """
+    do here: https://leetcode.com/problems/subtree-of-another-tree/
+    :type root: TreeNode
+    :type subRoot: TreeNode
+    :rtype: bool
+    """
