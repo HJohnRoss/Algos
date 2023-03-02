caa5ba648e0a02f4fd318b8c821811b518c1cb26
FJohnHRoss <rossjohn936@gmail.com>
2023-03-01 21:56:49 -0800

    update

---
+++ week4/w4d4/w4d4.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ * 
+ * Given the root of a binary tree, invert the tree, and return its root.
+ * 
+ * https://leetcode.com/problems/invert-binary-tree/
+ * Solution: https://youtu.be/OnSn2XEQ4MY
+ */
+
+// do inside of leetcode
+var invertTree = function (root) {
+  // do in leetcode: https://leetcode.com/problems/invert-binary-tree/
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ * 
+ * Given the root of a binary tree, return its maximum depth.
+ * 
+ * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ * 
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/
+ * Solution: https://youtu.be/OnSn2XEQ4MY
+ */
+var maxDepth = function (root) {
+  // Do on leetcode https://leetcode.com/problems/maximum-depth-of-binary-tree/
+  };
---
+++ week4/w4d4/w4d4.py
@@ -0,0 +1,49 @@
+# Definition for a binary tree node.
+# function TreeNode(val, left, right) {
+#     this.val = (val===undefined ? 0 : val)
+#     this.left = (left===undefined ? null : left)
+#     this.right = (right===undefined ? null : right)
+#
+#
+#
+# @param {TreeNode} root
+# @return {TreeNode}
+# 
+# Given the root of a binary tree, invert the tree, and return its root.
+# 
+# https://leetcode.com/problems/invert-binary-tree/
+# Solution: https://youtu.be/OnSn2XEQ4MY
+
+# do inside of leetcode
+def invertTree(root):
+  """
+  do in leetcode: https://leetcode.com/problems/invert-binary-tree/
+  :type root: TreeNode
+  :rtype: TreeNode
+  """
+  
+# * Definition for a binary tree node.
+# * function TreeNode(val, left, right) {
+# *     this.val = (val===undefined ? 0 : val)
+# *     this.left = (left===undefined ? null : left)
+# *     this.right = (right===undefined ? null : right)
+# * }
+# *
+# * @param {TreeNode} root
+# * @return {number}
+# * 
+# * Given the root of a binary tree, return its maximum depth.
+# * 
+# * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+# * 
+# * https://leetcode.com/problems/maximum-depth-of-binary-tree/
+# * Solution: https://youtu.be/OnSn2XEQ4MY
+
+
+# Do on leetcode https://leetcode.com/problems/maximum-depth-of-binary-tree/
+def maxDepth(root):
+  """
+  do in leetcode: https://leetcode.com/problems/invert-binary-tree/
+  :type root: TreeNode
+  :rtype: int
+  """
