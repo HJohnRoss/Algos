54c72fb633497e2c2c45eb6e880827cde4b0d743
FJohnHRoss <rossjohn936@gmail.com>
2023-03-08 08:57:34 -0800

    w5d3

--- week5/w5d3/w5d3.js
+++ week5/w5d3/w5d3.js
@@ -12,15 +12,46 @@
  * solution: https://youtu.be/bNvIQI2wAjk
  */
 
-const nums1 = [1,2,3,4]
-const Output1 = [24,12,8,6]
+const nums1 = [1, 2, 3, 4]
+const Output1 = [24, 12, 8, 6]
 
-const nums2 = [-1,1,0,-3,3]
-const Output2 = [0,0,9,0,0]
+const nums2 = [-1, 1, 0, -3, 3]
+const Output2 = [0, 0, 9, 0, 0]
 
 var productExceptSelf = function (nums) {
-
+  let result = []
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 1
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j) {
+        sum *= nums[j]
+      }
+    }
+    result.push(sum)
+  }
+  return result
 };
 
-console.log(productExceptSelf(nums1))
-console.log(productExceptSelf(nums2))
+var productExceptSelf1 = function (nums) {
+  let result = []
+  let prefix = 1;
+  let postfix = 1;
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = prefix;
+    prefix *= nums[i];
+  }
+  console.log(result)
+  for (let i = nums.length - 2; i >= 0; i--) {
+    postfix *= nums[i + 1];
+    result[i] *= postfix;
+  }
+  return result
+};
+
+console.log(productExceptSelf1(nums1))
+console.log("-------------------")
+console.log(productExceptSelf1(nums2))
+
+// console.log(productExceptSelf(nums1))
+// console.log("-------------------")
+// console.log(productExceptSelf(nums2))
