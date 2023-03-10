10993253d1a3e290863e1e8c82129f9aea1dedee
FJohnHRoss <rossjohn936@gmail.com>
2023-03-10 08:23:28 -0800

    update

--- week5/w5d5/w5d5.js
+++ week5/w5d5/w5d5.js
@@ -10,15 +10,34 @@
  * solution: https://youtu.be/P6RZZMu_maU
  */
 
-const nums1 = [100,4,200,1,3,2]
+const nums1 = [100, 4, 200, 1, 3, 2]
 const Output1 = 4
 // Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
 
-const nums2 = [0,3,7,2,5,8,4,6,0,1]
+const nums2 = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
 const Output2 = 9
 
-var longestConsecutive = function(nums) {
+var longestConsecutive = function (nums, maxScore = 0) {
+  const numSet = new Set(nums);
 
+  for (const num of numSet) {
+    const prev = num - 1
+    if (numSet.has(prev)) {
+      continue
+    }
+
+    let [curr, score] = [num, 1]
+
+    const isOrder = () => numSet.has(curr + 1)
+    while (isOrder()) {
+      curr++
+      score++
+    }
+
+    maxScore = Math.max(maxScore, score)
+  }
+
+  return maxScore
 };
 
 console.log(longestConsecutive(nums1))
