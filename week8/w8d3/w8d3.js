3959157a6ab2ad015aea837615d6ffec6c41c1b9
FJohnHRoss <rossjohn936@gmail.com>
2023-03-29 08:01:44 -0700

    w8d3

---
+++ week8/w8d3/w8d3.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ * 
+ * Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+ * 
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/
+ * solution: https://youtu.be/6ZnyEApgFYg
+ */
+var levelOrder = function(root) {
+  // do in leetcode: https://leetcode.com/problems/binary-tree-level-order-traversal/
+};
