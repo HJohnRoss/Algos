94cb3984c138ddca903abba539b03716d9877f0d
FJohnHRoss <rossjohn936@gmail.com>
2023-03-31 08:28:19 -0700

    w8d5

--- week8/w8d5/w8d5.js
+++ week8/w8d5/w8d5.js
@@ -15,6 +15,25 @@
  * https://leetcode.com/problems/binary-tree-level-order-traversal/
  * solution: https://youtu.be/6ZnyEApgFYg
  */
-var levelOrder = function(root) {
-  // do in leetcode: https://leetcode.com/problems/binary-tree-level-order-traversal/
+var levelOrder = function (root, level = 0, arr = []) {
+  if (!root) {
+    return arr
+  }
+  if (level === arr.length) {
+    arr.push([])
+  }
+
+  arr[level].push(root.val)
+
+  return leftRight(root, level, arr)
 };
+
+const leftRight = (root, level, arr) => {
+  if (root.left) {
+    levelOrder(root.left, (level + 1), arr)
+  }
+  if (root.right) {
+    levelOrder(root.right, (level + 1), arr)
+  }
+  return arr
+}
