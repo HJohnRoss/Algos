8955cb537442fb9bd55aa5012bd1518571bd1515
FJohnHRoss <rossjohn936@gmail.com>
2023-04-19 08:48:59 -0700

    w11d3

---
+++ week11/w11d2/w11d3.py
@@ -0,0 +1,23 @@
+# * Definition for singly-linked list.
+# * function ListNode(val, next) {
+# *     this.val = (val===undefined ? 0 : val)
+# *     this.next = (next===undefined ? null : next)
+# * }
+
+# * @param {ListNode} l1
+# * @param {ListNode} l2
+# * @return {ListNode}
+# * 
+# * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes
+# * contains a single digit. Add the two numbers and return the sum as a linked list.
+# * 
+# * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+# * 
+# * https://leetcode.com/problems/add-two-numbers/
+# * solution: https://youtu.be/wgFPrzTjm7s
+
+def addTwoNumbers(l1, l2):
+  """
+  do in leetcode:
+  https://leetcode.com/problems/add-two-numbers/
+  """
